0c5ee73079a1137d68499d79543da99db0418ef2
saroush5 <sarama1@ac.sce.ac.il>
2023-09-29 12:17:44 +0200

    modifs

--- frontend/src/Accueil.js
+++ frontend/src/Accueil.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './Accueil.css';
 
 function Accueil() {
+  // Fonction appelée lors du clic sur le bouton
+  const handleLearnMoreClick = () => {
+    // Redirection vers l'URL souhaitée
+    window.location.href = 'https://consultation.avocat.fr/avocat-bagnolet/dalia-mimoun-42248.html';
+  };
+
   return (
     <div className='accueil'>
         <br></br>
         <span>Maître Dalia MIMOUN exerce en tant qu'avocate au barreau de Bobigny, où elle a établi sa propre structure afin de consacrer son engagement quotidien au service de ses clients. Son cabinet est idéalement situé au cœur de Bagnolet.</span><br></br><br></br>
-        <a href="https://consultation.avocat.fr/avocat-bagnolet/dalia-mimoun-42248.html" className="btn">Planifier un rendez-vous</a>
+        <button onClick={handleLearnMoreClick}> Planifier un rendez-vous </button>
     </div>
   );
 }
--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -4,12 +4,12 @@ function Footer() {
     const currentYear = new Date().getFullYear();
   
     return (
-      <footer>
+      <div className='foot'>
         <p className='footer'>Maître Dalia MIMOUN<br></br></p>
         <p className='footer'>73 rue Pierre et Marie, 93170 Bagnolet<br></br></p>
         <p className='footer'><a href='tel:+33119881505' className='mail'>01.19.88.15.05</a> ~ <a href='mailto:avocat@mimoun.org' className='mail'>avocat@mimoun.org</a><br></br></p>
         <p className='droits'>Tous droits réservés | Dalia Mimoun © {currentYear}</p>
-      </footer>
+      </div>
     );
   }
   
