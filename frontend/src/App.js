32870c7a2ed11b1d0a5a4d6732a7dbbf25512bd6
saroush5 <sarama1@ac.sce.ac.il>
2023-10-04 02:25:21 +0200

    updates to put online

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { createContext, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createContext, useState } from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import Menu from './Menu';
-import Acces from './Acces'; 
-import Propos from './Propos'; 
-import Contact from './Contact'; 
+import Acces from './Acces';
+import Propos from './Propos';
+import Contact from './Contact';
 import Prestations from './Prestations';
 import Accueil from './Accueil';
 
@@ -18,20 +18,20 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-          <Menu/>
-          <Routes>
-            <Route path="/" element={<Accueil />} />
-            <Route path="/acces" element={<Acces />} />
-            <Route path="/propos" element={<Propos />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/prestations" element={<Prestations />} />
-          </Routes>
-          <div className="container">
-            <img src="../bob.png" alt="Image" className="bobi" />
-          </div>
-          <Footer />
-          </div>
-    </Router> 
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/acces" element={<Acces />} />
+          <Route path="/propos" element={<Propos />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/prestations" element={<Prestations />} />
+        </Routes>
+        <div className="container">
+          <img src="../bob.png" alt="Image" className="bobi" />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
